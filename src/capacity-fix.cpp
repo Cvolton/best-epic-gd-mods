cbfcd4786cb3658c01f846daacec8c2f9160d1f9
Cvolton <cvolton@cvolton.eu>
2022-02-24 22:35:47 +0100

    capacity-fix: Initial version

---
+++ src/capacity-fix.cpp
@@ -0,0 +1,31 @@
+#include <Windows.h>
+
+const unsigned char node_offset = 0x1B;
+const size_t base_address_21 = 0x10C582 - (node_offset * 0x10);
+
+DWORD WINAPI func(void* me) {
+    auto proc = GetCurrentProcess();
+    auto base = reinterpret_cast<char*>(GetModuleHandle(NULL));
+    //2.0 fix
+    static const size_t addresses[] = {0x10C519, 0x10C534, 0x10C560};
+    for(unsigned char i = 0; i < sizeof(addresses); i++){
+        unsigned char patch[] = {static_cast<unsigned char>(i + 0x0D)};
+        WriteProcessMemory(proc, base + addresses[i], patch, 1, NULL);
+    }
+    //2.1 fix
+    for(unsigned char i = 0x10; i < 53; i++){
+        size_t address = (node_offset * i) + base_address_21;
+        unsigned char patch[] = {i};
+        WriteProcessMemory(proc, base + address, patch, 1, NULL);
+    }
+
+    FreeLibraryAndExitThread(reinterpret_cast<HMODULE>(me), 0);
+    return 0;
+}
+
+BOOL APIENTRY DllMain(HMODULE mod, DWORD reason, LPVOID) {
+    if (reason == DLL_PROCESS_ATTACH)
+        if (auto h = CreateThread(NULL, 0, &func, mod, 0, NULL))
+            CloseHandle(h);
+    return TRUE;
+}
