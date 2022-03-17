e28b95c00b541a4ce0c9b97ea3f2a11c28d14e2c
Cvolton <cvolton@cvolton.eu>
2022-03-17 10:51:21 +0100

    geometricka-pomlcka: Fix capitalization

--- src_other/geometricka-pomlcka.cpp
+++ src_other/geometricka-pomlcka.cpp
@@ -4,17 +4,8 @@
 #define _UNICODE
 
 DWORD WINAPI func(void* me) {
-    /*auto proc = GetCurrentProcess();
-    auto base = reinterpret_cast<char*>(GetModuleHandle(NULL));
-    //This patches the maximum number for SetIDPopup to INT_MAX
-    unsigned char patch[] = {0x68, 0xFF, 0xFF, 0xFF, 0x7F};
-    WriteProcessMemory(proc, base + 0x1431F6, patch, 5, NULL);
-    //This patches the amount of characters allowed in the text input in SetIDPopup to 6 characters
-    unsigned char patch2[] = {0x06};
-    WriteProcessMemory(proc, base + 0x14371C, patch2, 1, NULL);
-    FreeLibraryAndExitThread(reinterpret_cast<HMODULE>(me), 0);*/
     auto hwnd = FindWindowW(NULL, L"Geometry Dash");
-    SetWindowTextW(hwnd, L"Geometrická Pomlèka");
+    SetWindowTextW(hwnd, L"Geometrická pomlèka");
     return 0;
 }
 
