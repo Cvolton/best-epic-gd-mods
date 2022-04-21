89a510f3bba6a7433052d83ac094a67da1ec9f1a
Cvolton <cvolton@cvolton.eu>
2022-04-21 16:16:08 +0200

    Fix DailyListView overflow

--- src/layers/DailyListView.cpp
+++ src/layers/DailyListView.cpp
@@ -21,6 +21,9 @@ void DailyListView::setupList() {
 
     this->m_pTableView->reloadData();
 
+    auto coverage = calculateNodeCoverage(m_pTableView->m_pContentLayer->getChildren());
+    m_pTableView->m_pContentLayer->setContentSize({-coverage.origin.x + coverage.size.width, -coverage.origin.y + coverage.size.height});
+
     this->m_pTableView->moveToTop();
 
     if (this->m_pEntries->count() == 1)
@@ -36,4 +39,43 @@ void DailyListView::loadCell(TableViewCell* cell, unsigned int index) {
 
 TableViewCell* DailyListView::getListCell(const char* key) {
     return DailyCell::create(key, { this->m_fWidth, 55.0f });
+}
+
+/*
+    https://github.com/geode-sdk/loader/blob/77c47653778db8c77d86e483996bc84b7dad26f7/src/utils/node.cpp#L6
+    will switch to geode implementation in geode version assuming that ever happens
+    (scuffed rushed re-implementation below)
+*/
+CCRect DailyListView::calculateNodeCoverage(CCArray* nodes){
+    CCRect coverage;
+    CCObject* childObj;
+    CCARRAY_FOREACH(nodes, childObj) {
+        CCNode* child = dynamic_cast<CCNode*>(childObj);
+        if(child == nullptr) continue;
+
+        auto contentSize = child->getScaledContentSize();
+        contentSize.width *= child->getAnchorPoint().x;
+        contentSize.height *= child->getAnchorPoint().y;
+        auto pos = child->getPosition() - contentSize;
+
+        contentSize = child->getScaledContentSize();
+        auto contentPoint = CCPoint { 1.f, 1.f } - child->getAnchorPoint();
+        contentSize.width *= contentPoint.x;
+        contentSize.height *= contentPoint.y;
+
+        auto csize = child->getPosition() + contentSize;
+        if (pos.x < coverage.origin.x) {
+            coverage.origin.x = pos.x;
+        }
+        if (pos.y < coverage.origin.y) {
+            coverage.origin.y = pos.y;
+        }
+        if (csize.x > coverage.size.width) {
+            coverage.size.width = csize.x;
+        }
+        if (csize.y > coverage.size.height) {
+            coverage.size.height = csize.y;
+        }
+    }
+    return coverage;
 }
--- src/layers/DailyListView.h
+++ src/layers/DailyListView.h
@@ -7,6 +7,7 @@ protected:
     void setupList() override;
     gd::TableViewCell* getListCell(const char* key) override;
     void loadCell(gd::TableViewCell* cell, unsigned int index) override;
+    static cocos2d::CCRect calculateNodeCoverage(cocos2d::CCArray* nodes);
     
 public:
     static DailyListView* create(cocos2d::CCArray* levels, float width, float height);
