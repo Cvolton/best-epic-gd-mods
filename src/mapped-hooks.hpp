0acc39d29f1f42c01218b888df29d508319b9f5d
Cvolton <cvolton@cvolton.eu>
2022-06-27 11:43:05 +0200

    Update hook error message

--- src/mapped-hooks.hpp
+++ src/mapped-hooks.hpp
@@ -12,7 +12,7 @@ namespace MHook {
         auto status = MH_CreateHook(reinterpret_cast<void**>(address), hook, &trampoline);
         if (status == MH_OK)
             hooks[hook] = trampoline;
-        else if(MessageBoxA(nullptr, "An error has occured while hooking a function...\n\nThis is likely caused by an outdated version of \"minhook.x86.dll\".\nWould you like to open the download", "BetterInfo - Geometry Dash", MB_ICONERROR | MB_YESNO) == IDYES) system("explorer https://github.com/HJfod/minhook/releases/latest");
+        else if(MessageBoxA(nullptr, "An error has occured while hooking a function...\n\nThis is likely caused by an outdated version of \"minhook.x86.dll\".\nWould you like to open the download page and get the updated version?\n\nFix tutorial:\n1) Download minhook.x86.dll from this page\n2) Put it in your GD folder", "BetterInfo - Geometry Dash", MB_ICONERROR | MB_YESNO) == IDYES) system("explorer https://github.com/HJfod/minhook/releases/latest");
         return status;
     }
 
